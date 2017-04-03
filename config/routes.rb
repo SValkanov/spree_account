8cb952f38f5b0d75baccf185892f8271c19a2ee9
SValkanov <s.valkanov97@abv.bg>
2017-04-03 15:05:46 +0300

    remove invoice route from this gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Spree::Core::Engine.routes.draw do
   get 'account/orders' => 'users#orders'
   match 'account/addresses' => 'users#addresses', via: [:get,:put]
-  match 'account/invoices' => 'users#invoices', via: [:get, :put]
 end
